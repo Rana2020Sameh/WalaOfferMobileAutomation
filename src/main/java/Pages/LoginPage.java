e4ae1e59495d68c5c067a46e2b6b6d14a8db22da
RanaHefny <r.hefny@walaplus.com>
2025-07-30 09:13:02 +0300

    add login elements

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -7,9 +7,15 @@ public class LoginPage extends BasePage {
     
     // Flutter element locators using keys
     private static final String EMAIL_FIELD_KEY = "email_field";
+    private static final String MOBILENUMBER_FIELD_KEY="mobile_number";
     private static final String PASSWORD_FIELD_KEY = "password_field";
     private static final String LOGIN_BUTTON_KEY = "login_button";
     private static final String ERROR_MESSAGE_KEY = "error_message";
+    private static final String  PERMESSION_ALLOW_BUTTON="permission_allow_button";
+    private static final String  PERMESSION_NOTALLOW_BUTTON="permission_not_allow_button";
+    private static final String  START_NOW_BUTTON="start_now_button";
+    private static final String NEXT_BUTTON="newx_button";
+    private static final String LOGIN_BUTTON="login_button";
     
     public void enterEmail(String email) {
         WebElement emailField = findFlutterElement("key", EMAIL_FIELD_KEY);
